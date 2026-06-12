977b8506d2704a50d79f74d0193534cfe64fde35
yashmangla007 <yashmangla.official@gmail.com>
2026-06-13 01:53:13 +0530

    Corrected

--- basicSorting/recurssiveInsertionSort.cpp
+++ basicSorting/recurssiveInsertionSort.cpp
@@ -8,22 +8,19 @@ void swap(int* x, int* y){
     return;
 }
 
-void recInsertionSort(int arr[], int n){
-    if(n==1) return;
+void recInsertionSort(int arr[], int x, int n){
+    if(n==x) return;
 
-    int j = 0, didswap= 0;
-    while(j<n){
+    int j = n;
+    while(j>=0){
         if(arr[j]<arr[j-1]){
             swap(&arr[j], &arr[j-1]);
-            j++;
-            didswap++;
+            j--;
         }
         else break;
     }
 
-    if(didswap == 0) return;
-
-    recInsertionSort(arr, n+1);
+    recInsertionSort(arr, x, n+1);
     return;
 }
 
@@ -32,7 +29,7 @@ int main(){
     int arr[10] = { 55,12,54,9,2,17,84,96,722,0};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    recInsertionSort(arr, 2);
+    recInsertionSort(arr, n, 2);
 
     cout<<"Sorted Array: ";
     for(int i=0; i<n; i++){
