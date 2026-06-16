009d8b569254a251c65bc469fd7b53e45e287cfe
yashmangla007 <yashmangla.official@gmail.com>
2026-06-16 16:51:28 +0530

    MoveArrayby1.cpp

---
+++ arrays/easy/moveArrby1.cpp
@@ -0,0 +1,31 @@
+/*
+Problem Statement: Given an integer array nums, rotate the array to the left by one.
+*/
+
+#include<iostream>
+using namespace std;
+
+void moveArrbyOne(int nums[], int n){
+    int temp = nums[0];
+    for(int i = 0; i<n-1; i++){
+        nums[i] = nums[i+1]; 
+    }
+    nums[n-1] = temp;
+    return;
+}
+
+int main(){ 
+    int arr[]= {12, 55, 14, 8, 63, 775, 9665, 21, 85};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    moveArrbyOne(arr, n);
+
+    cout<<"Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+
+    return 0;
+}
