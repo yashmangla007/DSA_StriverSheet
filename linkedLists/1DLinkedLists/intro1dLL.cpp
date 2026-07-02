3a874751a17b7a32325cab0427f3b5c3a94eb36d
yashmangla007 <yashmangla.official@gmail.com>
2026-07-02 22:09:34 +0530

    LL- Intro1

---
+++ linkedLists/1DLinkedLists/intro1dLL.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node (int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+int main(){
+
+    vector<int> arr = {2,5, 7,8};
+    
+    Node* y = new Node(arr[0], nullptr);
+
+    
+    cout<<y<<'\n';
+    
+    cout<<y->data <<'\n';
+    
+    cout<< y->next ;
+
+    return 0;
+}
