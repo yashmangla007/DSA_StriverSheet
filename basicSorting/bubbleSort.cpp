08919cc479b128ae4fda0bea68504fff2cb866c6
yashmangla007 <yashmangla.official@gmail.com>
2026-07-12 14:59:44 +0530

    Corrected Bubble Sort

--- basicSorting/bubbleSort.cpp
+++ basicSorting/bubbleSort.cpp
@@ -14,20 +14,21 @@ int main(){
 
     int arr[10] = { 55,12,54,9,2,17,84,96,722,0};
 
-    int n = sizeof(arr)/sizeof(arr[0]), didSwap=0;
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-    for (int i = n - 1; i > 0; i--)
-    {
+    for (int i = n - 1; i > 0; i--){
+        bool  didSwap=false;
         for (int j = 0; j < i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(&arr[j], &arr[j + 1]);
-                didSwap++;      // for optimisation for Best case scenerio (early exit)
+                didSwap = true;      // for optimisation for Best case scenerio (early exit)
             }
         }
-        if (didSwap == 0)
+        if (!didSwap){
             break;
+        }
     }
 
     cout<<"Sorted Array: ";
