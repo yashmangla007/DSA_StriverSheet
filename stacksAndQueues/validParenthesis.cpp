8ce08729eebf0573ab2ad7d795dfab57b6da3b75
yashmangla007 <yashmangla.official@gmail.com>
2026-09-15 12:23:24 +0530

    Correct Valid Parethesis

--- stacksAndQueues/validParenthesis.cpp
+++ stacksAndQueues/validParenthesis.cpp
@@ -1,53 +1,36 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-#define MAX_SIZE 101
-char a[MAX_SIZE];
-int top = -1;
-
-void push(char ele){
-    if(top<=MAX_SIZE-1){
-        a[++top]=ele;
-    }
-    else{
-        cout<<"Stack is full. Cannot push: "<<ele<<"\n";
-    }
-}
-
-char pop(){
-    if(top>=0){
-        char ele = a[top];
-        top--;
-        return ele;
-    }
-    else{
-        cout<<"Stack is empty. Cannot pop.\n";
-        return '-1';
-    }
-}
-
-bool isEmpty(){
-    return top==-1;
-}
-
-bool isFull(){
-    return top>=MAX_SIZE;
-}
-
 bool isMatchingPair(char open, char close) {
-    return (open == '(' && close == ')');
+    if(open == '(') return (open == '(' && close == ')');
+    else if(open == '[') return (open == '[' && close == ']');
+    else return (open == '{' && close == '}');
 }
 
 bool isBalanced(string expression) {
-    top = -1;
-    for(int i=0; i<expression.size(); i++){
-        if(expression[i]=='(') push('(');
-        else if(!isEmpty()) pop();
-        else return false;
+    int n = expression.size();
+    
+    stack<char> st;
+    
+    for(int i=0; i<n; i++){
+        if(expression[i]=='(' 
+            || expression[i]=='['
+            || expression[i]=='{') {
+                
+                st.push(expression[i]);
+        }
+        
+        else if(expression[i]==')' 
+                || expression[i]==']'
+                || expression[i]=='}'){
+            
+            if(st.empty()) return false;
+            
+            else if(isMatchingPair(st.top(), expression[i])) st.pop();
+        }
     }
     
-    if(isEmpty()) return true;
-    else return false;
+    return st.empty();
 }
 
 int main(){
@@ -55,6 +38,7 @@ int main(){
     cin>>t;
 
     while(t--){
+        
         string str;
         cin>>str;
         int n = str.length();
